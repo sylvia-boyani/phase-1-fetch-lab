5169f8ac326e9ea9139ac260190f6dec8c68231e
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-17 06:48:23 +0300

    commit tree 3566677f260e12077b9fc51e30d6ab190a96178e

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
+const url = "https://anapioficeandfire.com/api/books";
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+  let result = fetch(url)
+    .then((response) => response.json())
+    .then((data) => renderBooks(data))
+    .catch((error) => `ERROR => ${console.error(error.message)}`);
+  return result;
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main")
+  books.forEach((book) => {
+    const h2 = document.createElement("h2")
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function(){
   fetchBooks();
 });
